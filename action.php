5b4625870d7a2cfce2c036a63bb7af6766f44516
lupo49 <dokuwiki@lupo49.de>
2013-04-07 21:42:25 +0200

    Initial commit

---
+++ action.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * DokuWiki Plugin singlesearchresult (Action Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Matthias Schulte <dokuwiki@lupo49.de>
+ */
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_LF')) define('DOKU_LF', "\n");
+if(!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
+
+require_once DOKU_PLUGIN.'action.php';
+
+class action_plugin_singlesearchresult extends DokuWiki_Action_Plugin {
+
+    public function register(Doku_Event_Handler &$controller) {
+        $controller->register_hook('SEARCH_QUERY_FULLPAGE', 'AFTER', $this, 'handle_search_query_pagelookup');
+        //$controller->register_hook('ACTION_ACT_PREPROCESS', 'AFTER', $this, 'handle_search_query_pagelookup1');
+    }
+
+    // Bei einem Ergebnis ein trigger_event (ACTION_ACT_PREPROCESS) ausl�sen, dass ein redirect macht.?
+    
+    public function handle_search_query_pagelookup(Doku_Event &$event, $param) {
+        global $conf;
+        $result = $event->result;
+
+        // Only one page found, skip result overview and open the found page
+        if(count($result) == 1) {
+            $pageid = key($result);
+
+            if($_SERVER['REMOTE_USER']) {
+                $perm = auth_quickaclcheck($pageid);
+            } else {
+                $perm = auth_aclcheck($pageid, '', null);
+            }
+
+            if($perm > AUTH_NONE) {
+                if($conf['allowdebug']) {
+                    msg("Only one page found, skipping result overview. Redirect to: " . $pageid);
+                }
+                $link = wl($pageid, '', true);
+                print "<script type='text/javascript'>window.location.href='$link'</script>";
+            }
+        }
+    }
+}
+
+// vim:ts=4:sw=4:et:
